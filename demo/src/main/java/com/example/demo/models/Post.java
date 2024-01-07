5bb0053d1e1e4bf2971356ddc700eea4451d13e8
Angel_PC <araozangel842@gmail.com>
2024-01-07 17:16:43 +0200

    Post object

---
+++ demo/src/main/java/com/example/demo/models/Post.java
@@ -0,0 +1,35 @@
+package com.example.demo.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Document(collection = "posts")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Post {
+    @Id
+    private ObjectId id;
+    private String title;
+    private String body;
+    private String authorId;
+    private String image;
+    private List<String> tags;
+    private List<String> likes;
+    @DocumentReference
+    private List<Review> reviewIds;
+    @CreatedDate
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
