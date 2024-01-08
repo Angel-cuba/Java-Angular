edd495d9a4c44743b25750e0157e5f5bb35d377f
Angel_PC <araozangel842@gmail.com>
2024-01-08 11:41:03 +0200

    Dtos

---
+++ demo/src/main/java/com/example/demo/models/dtos/PostRequest.java
@@ -0,0 +1,24 @@
+package com.example.demo.models.dtos;
+
+import com.example.demo.models.Review;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PostRequest {
+    private String id;
+    private String title;
+    private String body;
+    private String authorId;
+    private String image;
+    private List<String> tags;
+    private List<String> likes;
+    private List<Review> reviewIds;
+    private LocalDateTime createdAt;
+}
---
+++ demo/src/main/java/com/example/demo/models/dtos/PostUpdateRequest.java
@@ -0,0 +1,19 @@
+package com.example.demo.models.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PostUpdateRequest {
+    private String title;
+    private String body;
+    private String image;
+    private List<String> tags;
+    private LocalDateTime updatedAt;
+}
