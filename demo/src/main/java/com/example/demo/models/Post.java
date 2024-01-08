180b9cb10c45ced6a1ed74b5a2e69733549ded7b
Angel_PC <araozangel842@gmail.com>
2024-01-08 11:39:59 +0200

    Delete unnecessary import

--- demo/src/main/java/com/example/demo/models/Post.java
+++ demo/src/main/java/com/example/demo/models/Post.java
@@ -3,7 +3,6 @@ package com.example.demo.models;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.bson.types.ObjectId;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
