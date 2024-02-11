8ec48beff1ca16fcf9f94487e1df2b4314e6bf14
Angel_PC <araozangel842@gmail.com>
2024-02-11 21:33:22 +0200

    Create authorized routes

--- demo/src/main/java/com/example/demo/SecurityConfig/securityConfig.java
+++ demo/src/main/java/com/example/demo/SecurityConfig/securityConfig.java
@@ -32,30 +32,42 @@ public class securityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http = http
-                .cors()
-                .and()
-                .csrf()
-                .disable();
-        http = http
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and();
+        String[] allowedUrls = new String[]{
+                //Users
+                "/api/v1/users/create",
+                "/api/v1/users/signing",
+                "/api/v1/users/all",
+                "/api/v1/users/user/{userId}",
+                "/api/v1/users/{id}/update",
+                "/api/v1/users/{id}/delete",
+                //Reviews
+                "/api/v1/reviews/all/{postId}",
+                "/api/v1/reviews/review/{reviewId}",
+                "/api/v1/reviews/create/{id}",
+                "/api/v1/reviews/{postId}/update/{reviewId}/{userId}",
+                "/api/v1/reviews/{postId}/delete/{reviewId}/{userId}",
+                //Posts
+                "/api/v1/posts/all",
+                "/api/v1/posts/{id}",
+                "/api/v1/posts/author/{authorId}",
+                "/api/v1/posts/create",
+                "/api/v1/posts/{postId}/like/{userId}",
+                "/api/v1/posts/{id}/update/{userId}",
+                "/api/v1/posts/{id}/delete/{userId}"
+        };
         http
-                .authorizeHttpRequests().requestMatchers("/api/v1/users/create", "/api/v1/users/signin")
-                .permitAll()
-                .requestMatchers(HttpMethod.GET, "/api/v1/users").permitAll()
-                .requestMatchers(HttpMethod.POST, "/api/v1/products").hasRole("ADMIN")
-                .requestMatchers(HttpMethod.PUT, "/api/v1/products").hasRole("ADMIN")
-                .requestMatchers(HttpMethod.PUT, "/api/v1/products/update/stock").permitAll()
-                .requestMatchers(HttpMethod.DELETE, "/api/v1/products/{id}").hasRole("ADMIN")
-                .requestMatchers(HttpMethod.GET, "/api/v1/users").hasRole("ADMIN")
-                .requestMatchers(HttpMethod.GET, "/api/v1/addresses").hasRole("ADMIN")
-                .requestMatchers(HttpMethod.GET, "/api/v1/payments").hasRole("ADMIN")
-                .requestMatchers(HttpMethod.GET, "/api/v1/wishes").hasRole("ADMIN")
-                .anyRequest()
-                .authenticated();
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeHttpRequests().requestMatchers("/api/v1/users/create", "/api/v1/users/signin").permitAll()
+                .requestMatchers(HttpMethod.POST, allowedUrls).permitAll()
+                .requestMatchers(HttpMethod.GET, allowedUrls).permitAll()
+                .requestMatchers(HttpMethod.PUT, allowedUrls).permitAll()
+                .requestMatchers(HttpMethod.DELETE, allowedUrls).permitAll()
+                .anyRequest().authenticated();
         http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
+
+
     }
 }
